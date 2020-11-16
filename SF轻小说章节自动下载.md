65204a7edf657dff15bbb23d794a686c1dbf13fe
NateScarlet <NateScarlet@Gmail.com>
2020-11-16 20:14:17 +0800

    feat: add SF轻小说章节自动下载.ts

---
+++ SF轻小说章节自动下载.md
@@ -0,0 +1,17 @@
+SF轻小说章节自动下载
+===========================
+
+打开SF轻小说章节页面时自动保存文章到本地, 支持付费章节。
+
+下载格式为 Markdown， 图片使用 data URL 形式存储。
+
+不熟悉此格式的用户可以安装对应的浏览器扩展后用浏览器打开:
+
+[火狐扩展](https://addons.mozilla.org/zh-CN/firefox/addon/markdown-viewer-webext/)
+
+[Chrome 扩展](https://chrome.google.com/webstore/detail/markdown-viewer/ckkdlimhmcjmikdlpkmbgfkaikojcbjk?hl=zh-CN) (需要设置 扩展程序 - markdown-viewer - 详细信息 - 允许访问文件网址)
+
+同样功能的其他网站版本
+----------------------
+
+[刺猬猫](https://greasyfork.org/zh-CN/scripts/381067-%E5%88%BA%E7%8C%AC%E7%8C%AB%E7%AB%A0%E8%8A%82%E8%87%AA%E5%8A%A8%E4%B8%8B%E8%BD%BD)
---
+++ SF轻小说章节自动下载.ts
@@ -0,0 +1,93 @@
+// ==UserScript==
+// @namespace https://github.com/NateScarlet/Scripts/tree/master/user-script
+// @name     SF轻小说章节自动下载
+// @description 打开SF轻小说章节页面时自动保存文章到本地, 支持付费章节。
+// @version  1
+// @grant    none
+// @include	 http://book.sfacg.com/Novel/*/*/*/
+// @include	 http://book.sfacg.com/vip/c/*/
+// @run-at   document-idle
+// ==/UserScript==
+
+export {};
+
+const __name__ = "SF轻小说章节自动下载";
+
+function image2line(img: HTMLImageElement, bgStyle?: string): string {
+  const canvas = document.createElement("canvas");
+  canvas.width = img.naturalWidth;
+  canvas.height = img.naturalHeight;
+  const ctx = canvas.getContext("2d");
+  if (bgStyle) {
+    ctx.fillStyle = bgStyle;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+  ctx.drawImage(img, 0, 0);
+  return `![${img.alt}](${canvas.toDataURL()} "${img.title}")`;
+}
+
+function strip(str: string): string {
+  return str.replace(/^\s+|\s+$/g, "");
+}
+
+function getElementRootText(element: Element): string {
+  let ret = "";
+  for (const i of element.childNodes) {
+    if (i.nodeType === i.TEXT_NODE) {
+      ret += i.nodeValue;
+    }
+  }
+  return strip(ret);
+}
+
+(async function (): Promise<void> {
+  const chapter = document.querySelector("#article .article-title").textContent;
+  let lines = [
+    `# ${chapter}`,
+    `[原始页面](${location.href})`,
+    ...document
+      .querySelector("#article .article-desc")
+      .textContent.split(/\n */)
+      .filter((i) => i),
+    `---`,
+  ];
+  const keywords = document
+    .querySelector<HTMLMetaElement>("meta[name='keywords']")
+    .content.split(",")
+    .filter((i) => !["小说下载", "TXT"].includes(i))
+    .filter((i, index, keywords) => index === 0 || !i.startsWith(keywords[0]));
+
+  // 收费章节
+  for (const i of document.querySelectorAll<HTMLImageElement>("img#vipImage")) {
+    console.log(`${__name__}: 等待图片加载`);
+    await i.decode();
+    console.log(`${__name__}: 图片加载完毕`);
+    const line = image2line(i, "white");
+    lines.push(line);
+  }
+  // 免费章节
+  for (const i of document.querySelectorAll("#ChapterBody p")) {
+    const line = getElementRootText(i);
+    lines.push(line);
+    for (const img of i.querySelectorAll("img")) {
+      lines.push(image2line(img));
+    }
+  }
+
+  // 下载文件
+  lines = lines.filter((i) => i.length > 0);
+  console.log(`${__name__}: 获取到 ${lines.length} 行`);
+  const file = new Blob([lines.join("\n\n"), "\n"], {
+    type: "text/markdown",
+  });
+  const anchor = document.createElement("a");
+  anchor.href = URL.createObjectURL(file);
+  anchor.download = `${keywords.join(" ")}.md`;
+  anchor.style["display"] = "none";
+  document.body.append(anchor);
+  anchor.click();
+  setTimeout(() => {
+    document.body.removeChild(anchor);
+    URL.revokeObjectURL(anchor.href);
+  }, 0);
+})();
