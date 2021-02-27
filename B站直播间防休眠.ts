f2f4fc9833020649e63e353bb5503c7fc528637b
NateScarlet <NateScarlet@Gmail.com>
2021-02-27 16:23:11 +0800

    feat(B站直播间防休眠): init

---
+++ B站直播间防休眠.ts
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @namespace https://github.com/NateScarlet/Scripts/tree/master/user-script
+// @name     B站直播间防休眠
+// @description 防止B站直播间自动停止播放
+// @version  1
+// @grant    none
+// @include	 https://live.bilibili.com/*
+// @include	 https://www.bilibili.com/blackboard/live/
+// @run-at   document-idle
+// ==/UserScript==
+
+(() => {
+  setInterval(() => {
+    setTimeout(() => {
+      document.body.dispatchEvent(
+        new MouseEvent("mousemove", { bubbles: true })
+      );
+    }, Math.random() * 60e3);
+  }, 600e3);
+})();
