aceb58ea12690a00983ca4ffdeb8f6c1327d826f
NateScarlet <NateScarlet@Gmail.com>
2021-02-27 16:30:31 +0800

    fix(B站直播间防休眠): correct rule

--- dist/B站直播间防休眠.js
+++ dist/B站直播间防休眠.js
@@ -3,10 +3,10 @@
 // @namespace https://github.com/NateScarlet/Scripts/tree/master/user-script
 // @name     B站直播间防休眠
 // @description 防止B站直播间自动停止播放
-// @version  1
+// @version  2
 // @grant    none
 // @include	 https://live.bilibili.com/*
-// @include	 https://www.bilibili.com/blackboard/live/
+// @include	 https://www.bilibili.com/blackboard/live/*
 // @run-at   document-idle
 // ==/UserScript==
 (() => {
