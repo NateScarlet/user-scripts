0d89be5c25890fffdc896d1ff4252f043c59dd6b
NateScarlet <NateScarlet@Gmail.com>
2021-03-26 22:28:08 +0800

    feat(B站直播间防休眠): block visibilitychange detect

--- B站直播间防休眠.ts
+++ B站直播间防休眠.ts
@@ -2,7 +2,7 @@
 // @namespace https://github.com/NateScarlet/Scripts/tree/master/user-script
 // @name     B站直播间防休眠
 // @description 防止B站直播间自动停止播放
-// @version  2
+// @version  3
 // @grant    none
 // @include	 https://live.bilibili.com/*
 // @include	 https://www.bilibili.com/blackboard/live/*
@@ -17,4 +17,11 @@
       );
     }, Math.random() * 60e3);
   }, 600e3);
+  window.addEventListener(
+    "visibilitychange",
+    (e) => {
+      e.stopPropagation();
+    },
+    { capture: true }
+  );
 })();
--- dist/B站直播间防休眠.js
+++ dist/B站直播间防休眠.js
@@ -3,7 +3,7 @@
 // @namespace https://github.com/NateScarlet/Scripts/tree/master/user-script
 // @name     B站直播间防休眠
 // @description 防止B站直播间自动停止播放
-// @version  2
+// @version  3
 // @grant    none
 // @include	 https://live.bilibili.com/*
 // @include	 https://www.bilibili.com/blackboard/live/*
@@ -15,4 +15,7 @@
             document.body.dispatchEvent(new MouseEvent("mousemove", { bubbles: true }));
         }, Math.random() * 60e3);
     }, 600e3);
+    window.addEventListener("visibilitychange", (e) => {
+        e.stopPropagation();
+    }, { capture: true });
 })();
