578e66d6797fa0311f5868bdaad048f4f9b492fe
NateScarlet <NateScarlet@Gmail.com>
2021-03-27 19:47:21 +0800

    feat(B站直播间防休眠): use smaller interval

--- B站直播间防休眠.ts
+++ B站直播间防休眠.ts
@@ -2,7 +2,7 @@
 // @namespace https://github.com/NateScarlet/Scripts/tree/master/user-script
 // @name     B站直播间防休眠
 // @description 防止B站直播间自动停止播放
-// @version  3
+// @version  4
 // @grant    none
 // @include	 https://live.bilibili.com/*
 // @include	 https://www.bilibili.com/blackboard/live/*
@@ -15,8 +15,8 @@
       document.body.dispatchEvent(
         new MouseEvent("mousemove", { bubbles: true })
       );
-    }, Math.random() * 60e3);
-  }, 600e3);
+    }, Math.random() * 2e3);
+  }, 10e3);
   window.addEventListener(
     "visibilitychange",
     (e) => {
--- dist/B站直播间防休眠.js
+++ dist/B站直播间防休眠.js
@@ -3,7 +3,7 @@
 // @namespace https://github.com/NateScarlet/Scripts/tree/master/user-script
 // @name     B站直播间防休眠
 // @description 防止B站直播间自动停止播放
-// @version  3
+// @version  4
 // @grant    none
 // @include	 https://live.bilibili.com/*
 // @include	 https://www.bilibili.com/blackboard/live/*
@@ -13,8 +13,8 @@
     setInterval(() => {
         setTimeout(() => {
             document.body.dispatchEvent(new MouseEvent("mousemove", { bubbles: true }));
-        }, Math.random() * 60e3);
-    }, 600e3);
+        }, Math.random() * 2e3);
+    }, 10e3);
     window.addEventListener("visibilitychange", (e) => {
         e.stopPropagation();
     }, { capture: true });
